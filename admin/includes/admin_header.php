80aafc1ff3377f5d62f22c3700ac67b1bf94fd20
Nic Bade <chipchapmaniii@gmail.com>
2018-07-04 12:18:27 -0500

    added an editor to add post

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -44,5 +44,11 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    
+    <script src="js/jquery.js"></script>
+
+    <script src="js/scripts.js"></script>
+    
+    <script src="https://cdn.ckeditor.com/ckeditor5/10.1.0/classic/ckeditor.js"></script>
 
 </head>
---
+++ admin/js/scripts.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+
+    ClassicEditor
+    .create( document.querySelector( '#body' ) )
+    .catch( error => {
+    console.error( error );
+    } );
+
+
+
+
+});
